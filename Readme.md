3a61f576f0c2c4816ae95eba7663f64d17eb2cd6
tamilrajendran <tamilarasi.rajendr@gmail.com>
2024-11-30 02:47:15 +0530

    updated

---
+++ Readme.md
@@ -0,0 +1,4 @@
+This is Javascript day 2. The Content are listed below
+# variables & Datatypes
+# Alert, Prompts, Confirm
+# Operators
---
+++ script.js
@@ -0,0 +1,56 @@
+console.log("Welcome to Javascript")
+/*
+//Var Examples
+var a = 10;
+a=20;//reassign
+var a= 30;//redeclare
+console.log(a);
+*/
+
+/*
+// Let Examples
+let b = 10;
+b=40; //reassign 
+console.log(b);
+*/
+
+/*
+// Const Examples
+const c = 10;
+console.log(c);
+*/
+
+/*
+//window method/events
+alert("Click Ok to enter into the website")
+let result = prompt("Enter your Name ")
+console.log(result)
+let result1 = confirm("Are you sure enter the website")
+console.log(result1)
+*/
+
+/*
+Undefined
+Let a;
+console.log(a);
+*/
+
+/*
+// Null
+Let num = null;
+console.log(num)
+console.log(typeof(num))
+*/
+
+/*
+//Object Examples
+let person={
+    firstname:"John",
+    lastname:"Doe",
+    age:35,
+    Sex:"Male",
+    phonenumber:9078563412,
+    skills:["Javascipt","Html","CSS"]
+}
+console.log(person)
+*/
